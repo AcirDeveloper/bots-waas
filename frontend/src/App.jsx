ee51d8dcd299b1ae0f1fd211f4e720211cda9ab9
Acir Dev <acirdeveloper@gmail.com>
2023-05-01 22:48:20 -0500

    pruebas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1>Hola mundo</h1>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam aliquam tempora quasi
+      ratione dolorum cumque nihil adipisci, fuga eum illum praesentium facere quibusdam architecto
+      alias, velit dolorem sunt ipsa. Quis.
     </>
   )
 }
